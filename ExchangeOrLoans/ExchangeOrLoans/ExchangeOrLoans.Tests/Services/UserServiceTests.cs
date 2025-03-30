9ff050cd718517e017011bb1ff64e3375506733d
Matheus Bernardo <matheus.mh@ges.inatel.br>
2025-03-30 17:18:30 -0300

    :test_tube: Tests UserService

--- ExchangeOrLoans/ExchangeOrLoans/ExchangeOrLoans.Tests/Services/UserServiceTests.cs
+++ ExchangeOrLoans/ExchangeOrLoans/ExchangeOrLoans.Tests/Services/UserServiceTests.cs
@@ -151,4 +151,125 @@ public class UserServiceTests
         Assert.Equal("Users not found", notFoundResult.Value);
     }
 
+    [Fact]
+    public async Task LoginUser_InvalidPassword_ShouldReturnBadRequest()
+    {
+      
+        var loginDto = new LoginDto
+        {
+            Email = "example@example.com",
+            Password = "wrongPassword"
+        };
+
+        var user = new User
+        {
+            Username = "exampleUser",
+            Email = "example@example.com",
+            FirstName = "Example",
+            LastName = "Example2",
+            Password = BCrypt.Net.BCrypt.HashPassword("correctPassword"),
+            Score = 1,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _userRepositoryMock.Setup(repo => repo.GetUserByEmail(loginDto.Email)).ReturnsAsync(user);
+
+        var service = new UserService(_userRepositoryMock.Object);
+        
+        var result = await service.Login(loginDto);
+        
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Password Incorrect", badRequestResult.Value);
+    }
+    
+    [Fact]
+    public async Task LoginUser_InvalidEmail_ShouldReturnBadRequest()
+    {
+       
+        var loginDto = new LoginDto
+        {
+            Email = "nonexistent@example.com",
+            Password = "anyPassword"
+        };
+
+      
+        _userRepositoryMock.Setup(repo => repo.GetUserByEmail(loginDto.Email)).ReturnsAsync((User)null!);
+
+        var service = new UserService(_userRepositoryMock.Object);
+        
+        var result = await service.Login(loginDto);
+        
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Email not found", badRequestResult.Value);
+    }
+
+    [Fact]
+    public async Task CreateUser_UsernameAlreadyExists_ShouldReturnBadRequest()
+    {
+        var newUser = new User
+        {
+            Username = "existingUser",
+            Email = "newuser@example.com",
+            Password = "validPassword123",
+            FirstName = "New",
+            LastName = "User"
+        };
+
+        _userRepositoryMock.Setup(repo => repo.UsernameExists(newUser.Username)).ReturnsAsync(true);
+
+        var service = new UserService(_userRepositoryMock.Object);
+        
+        var result = await service.CreateUser(newUser);
+        
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Username already exists", badRequestResult.Value);
+    }
+    
+    [Fact]
+    public async Task CreateUser_EmailAlreadyExists_ShouldReturnBadRequest()
+    {
+       
+        var newUser = new User
+        {
+            Username = "newusername",
+            Email = "existing@example.com",
+            Password = "validPassword123",
+            FirstName = "New",
+            LastName = "User"
+        };
+
+        _userRepositoryMock.Setup(repo => repo.UsernameExists(newUser.Username)).ReturnsAsync(false);
+        _userRepositoryMock.Setup(repo => repo.EmailExists(newUser.Email)).ReturnsAsync(true);
+
+        var service = new UserService(_userRepositoryMock.Object);
+        
+        var result = await service.CreateUser(newUser);
+        
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Email already exists", badRequestResult.Value);
+    }
+    
+    [Fact]
+    public async Task CreateUser_PasswordIsNullOrEmpty_ShouldReturnBadRequest()
+    {
+        
+        var newUser = new User
+        {
+            Username = "uniqueUser",
+            Email = "unique@example.com",
+            Password = "",
+            FirstName = "Empty",
+            LastName = "Password"
+        };
+
+        _userRepositoryMock.Setup(repo => repo.UsernameExists(newUser.Username)).ReturnsAsync(false);
+        _userRepositoryMock.Setup(repo => repo.EmailExists(newUser.Email)).ReturnsAsync(false);
+
+        var service = new UserService(_userRepositoryMock.Object);
+        
+        var result = await service.CreateUser(newUser);
+        
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Password is required", badRequestResult.Value);
+    }
 }
